a229df7876af1da294f4376883235ba572f3aa50
Robert Bitterling <robert@dedica.team>
2024-06-22 23:02:23 +0200

    Add GitLab support

--- client_impl/__init__.py
+++ client_impl/__init__.py
@@ -3,14 +3,18 @@ from typing import Optional
 
 from abstractions import ScmClient
 from client_impl.github_client import GitHubClient
+from client_impl.gitlab_client import GitLabClient
 
 
 class ScmClientImpl(StrEnum):
     GitHub = "github"
+    GitLab = "gitlab"
 
 
 def scm_client_factory(scm_provider: ScmClientImpl, pat: str, api_base_url: Optional[str] = None) -> ScmClient:
     if scm_provider == ScmClientImpl.GitHub:
         return GitHubClient(pat, api_base_url)
+    elif scm_provider == ScmClientImpl.GitLab:
+        return GitLabClient(pat, api_base_url)
     else:
         raise ValueError(f"Unknown SCM provider: {scm_provider}")
