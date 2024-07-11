51045556e504ff27dd082e29483b8d58d5d970f8
MShekow <s2mashek@uni-bonn.de>
2024-07-11 19:57:21 +0200

    Fix pagination for GitLab API "list()" calls

--- client_impl/gitlab_client.py
+++ client_impl/gitlab_client.py
@@ -40,10 +40,10 @@ class GitLabClient(ScmClient):
         if owner_or_username.startswith("user:"):
             owner_or_username = owner_or_username[5:]
             if self.get_username().lower() == owner_or_username.lower():
-                projects = self._gitlab_client.projects.list(owned=True)
+                projects = self._gitlab_client.projects.list(owned=True, get_all=True)
             else:
-                user = self._gitlab_client.users.list(username=owner_or_username)[0]
-                projects = self._gitlab_client.projects.list(user_id=user.id)
+                user = self._gitlab_client.users.list(username=owner_or_username, get_all=True)[0]
+                projects = self._gitlab_client.projects.list(user_id=user.id, get_all=True)
         else:
             group = self._gitlab_client.groups.get(owner_or_username)
             projects = self._get_projects_from_groups_recursive(group)
@@ -56,13 +56,13 @@ class GitLabClient(ScmClient):
     def _get_projects_from_groups_recursive(self, group: gitlab.v4.objects.Group) -> List[gitlab.v4.objects.Project]:
         # Convert subproject objects (stored in group.projects) to "full" GitLab Project objects, which also have
         # the mergerequests attribute
-        subprojects = group.projects.list()
+        subprojects = group.projects.list(get_all=True)
         projects: List[gitlab.v4.objects.Project] = []
         for sub_project in subprojects:
             projects.append(self._gitlab_client.projects.get(sub_project.id))
 
         # Recursively get projects from subgroups
-        subgroups: List[gitlab.v4.objects.GroupSubgroup] = group.subgroups.list()
+        subgroups: List[gitlab.v4.objects.GroupSubgroup] = group.subgroups.list(get_all=True)
         for subgroup in subgroups:
             full_subgroup = self._gitlab_client.groups.get(subgroup.id)
             projects.extend(self._get_projects_from_groups_recursive(full_subgroup))
@@ -93,7 +93,7 @@ class GitLabRepository(GitRepository):
         self._gitlab_client = gitlab_client
 
     def get_commits(self, since: datetime) -> List[GitCommit]:
-        gl_commits: List[ProjectCommit] = self._gl_project.commits.list(since=since.isoformat())
+        gl_commits: List[ProjectCommit] = self._gl_project.commits.list(since=since.isoformat(), get_all=True)
         gl_commits.reverse()
         commits: List[GitLabCommit] = [
             GitLabCommit(sha=commit.attributes['id'],
